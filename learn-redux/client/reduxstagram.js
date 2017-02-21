b2d1d1d52db86d6f9f0b78e58e25bdccbe80dd37
Steven Knight <steven.knight@cashstar.com>
2017-02-21 12:48:24 -0500

    Update for step 2.

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,10 @@
-// let's go!
+import React from 'react';
+import {render} from 'react-dom';
+
+//noinspection ES6UnusedImports
+import css from './styles/style.styl';
+
+import Main from './components/Main';
+
+render(<Main />,
+    document.getElementById('root'));
