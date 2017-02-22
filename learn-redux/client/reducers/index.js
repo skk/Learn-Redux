f5485721e7d3ae4b653938e90e0e0f7444571dd6
Steven Knight <steven.knight@cashstar.com>
2017-02-22 14:02:41 -0500

    Step 7.

---
+++ learn-redux/client/reducers/index.js
@@ -0,0 +1,15 @@
+
+import {combineReducers} from 'refux';
+import {routerReducer} from 'react-router-redux';
+
+import posts from './posts';
+import comments from './comments';
+
+const rootReducer = combineReducers({
+    posts,
+    comments,
+    routerReducer
+});
+
+
+export default rootReducer;
